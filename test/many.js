07fdd4d06918cb55bc1f056d46fd3087e470d9c6
James Halliday <mail@substack.net>
2013-03-07 19:06:21 -0800

    many test hangs on the event loop at the end

---
+++ test/many.js
@@ -0,0 +1,36 @@
+var test = require('tap').test;
+var http = require('http');
+var hreq = require('../');
+var through = require('through');
+
+var pending = [];
+var server = http.createServer(function (req, res) {
+    res.write('beep boop');
+    pending.push(res);
+});
+
+test('more than 5 pending connections', function (t) {
+    t.plan(20);
+    var pending = [];
+    server.listen(0, function () {
+        var port = server.address().port;
+        for (var i = 0; i < 20; i++) {
+            pending.push(check(t, port));
+        }
+    });
+    t.on('end', function () {
+        pending.forEach(function (p) { p.destroy() });
+        server.close();
+    });
+});
+
+function check (t, port) {
+    var r = hreq('http://localhost:' + port);
+    var data = '';
+    r.pipe(through(function (buf) { data += buf }));
+    
+    setTimeout(function () {
+        t.equal(data, 'beep boop');
+    }, 100);
+    return  r;
+}
