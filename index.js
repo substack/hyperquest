2944923b13e3c141a209ed58548af346475449a7
zensh <admin@zensh.com>
2014-10-20 23:25:41 +0800

    remove the slow bind

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ var duplexer = require('duplexer2');
 
 module.exports = hyperquest;
 
-function bind (obj, fn) {
-    var args = Array.prototype.slice.call(arguments, 2);
-    return function () {
-        var argv = args.concat(Array.prototype.slice.call(arguments));
-        return fn.apply(obj, argv);
-    }
-}
-
 function hyperquest (uri, opts, cb, extra) {
     if (typeof uri === 'object') {
         cb = opts;
@@ -27,48 +19,48 @@ function hyperquest (uri, opts, cb, extra) {
     if (!opts) opts = {};
     if (uri !== undefined) opts.uri = uri;
     if (extra) opts.method = extra.method;
-    
+
     var req = new Req(opts);
     var ws = req.duplex && through();
     var rs = through();
-    
+
     var dup = req.duplex ? duplexer(ws, rs) : rs;
     if (!req.duplex) {
         rs.writable = false;
     }
     dup.request = req;
-    dup.setHeader = bind(req, req.setHeader);
-    dup.setLocation = bind(req, req.setLocation);
-    
+    dup.setHeader = function () { req.setHeader.apply(req, arguments); };
+    dup.setLocation = function () { req.setLocation.apply(req, arguments); };
+
     var closed = false;
-    dup.on('close', function () { closed = true });
-    
+    dup.on('close', function () { closed = true; });
+
     process.nextTick(function () {
         if (closed) return;
-        dup.on('close', function () { r.destroy() });
-        
+        dup.on('close', function () { r.destroy(); });
+
         var r = req._send();
-        r.on('error', bind(dup, dup.emit, 'error'));
-        
+        r.on('error', function (error) { dup.emit('error', error); });
+
         r.on('response', function (res) {
             dup.response = res;
             dup.emit('response', res);
-            if (req.duplex) res.pipe(rs)
+            if (req.duplex) res.pipe(rs);
             else {
-                res.on('data', function (buf) { rs.push(buf) });
-                res.on('end', function () { rs.push(null) });
+                res.on('data', function (buf) { rs.push(buf); });
+                res.on('end', function () { rs.push(null); });
             }
         });
-        
+
         if (req.duplex) {
             ws.pipe(r);
         }
         else r.end();
     });
-    
+
     if (cb) {
         dup.on('error', cb);
-        dup.on('response', bind(dup, cb, null));
+        dup.on('response', function (res) { cb(null, res); });
     }
     return dup;
 }
@@ -89,28 +81,28 @@ hyperquest['delete'] = function (uri, opts, cb) {
 
 function Req (opts) {
     this.headers = opts.headers || {};
-    
+
     var method = (opts.method || 'GET').toUpperCase();
     this.method = method;
     this.duplex = !(method === 'GET' || method === 'DELETE'
         || method === 'HEAD');
     this.auth = opts.auth;
-    
+
     this.options = opts;
-    
+
     if (opts.uri) this.setLocation(opts.uri);
 }
 
 Req.prototype._send = function () {
     this._sent = true;
-    
+
     var headers = this.headers || {};
     var u = url.parse(this.uri);
     var au = u.auth || this.auth;
     if (au) {
         headers.authorization = 'Basic ' + Buffer(au).toString('base64');
     }
-    
+
     var protocol = u.protocol || '';
     var iface = protocol === 'https:' ? https : http;
     var opts = {
@@ -133,7 +125,7 @@ Req.prototype._send = function () {
         opts.secureProtocol = this.options.secureProtocol;
     }
     var req = iface.request(opts);
-    
+
     if (req.setTimeout) req.setTimeout(Math.pow(2, 32) * 1000);
     return req;
 };
