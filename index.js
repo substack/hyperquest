9b130e101faef3ec6ce4c89c2ff2b2b9062f5b58
James Halliday <mail@substack.net>
2016-07-02 19:13:25 -0700

    DELETE should not auto-close, may have a payload

--- index.js
+++ index.js
@@ -93,8 +93,7 @@ function Req (opts) {
     
     var method = (opts.method || 'GET').toUpperCase();
     this.method = method;
-    this.duplex = !(method === 'GET' || method === 'DELETE'
-        || method === 'HEAD');
+    this.duplex = !(method === 'GET' || method === 'HEAD');
     this.auth = opts.auth;
     
     this.options = opts;
